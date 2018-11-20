1d6c1271537e222ee63f2187e6e52f70fa5d4f28
XuLei <QianXunQingJie@outlook.com>
2018-11-20 18:19:46 +0800

    init

---
+++ xtstring.rb
@@ -0,0 +1,204 @@
+def  messaginfo(strinfo)
+	puts 'messagbyxdraw:'+strinfo
+end
+
+def  scopyright
+%q{2014,珠海橱动力科技  }
+end
+
+def  screator
+%q{Leimi Deng  }
+end
+
+def  sversion
+%q{2.09.13  }
+end
+
+def  sdescription
+%q{珠海橱动力科技为板式家具行业定制开发  www.chudl.com  }
+end
+
+def  scgpower
+%q{橱动力  }
+end
+
+def  sbillinfo
+%q{订单信息  }
+end
+
+def  sMeasureTool
+%q{测量尺寸  }
+end
+
+def  sDimensionTool
+%q{尺寸标注  }
+end
+
+def  sdrawcabinet
+%q{物件摆放  }
+end
+
+def sdrawlamp
+%q{灯线顶线  }
+end
+
+def  sreportcabinet
+%q{柜子数据报表  }
+end
+
+def  sGetBoardHole
+%q{孔位五金  }
+end
+
+def  sreporthole
+%q{数据输出  }
+end
+
+def  stesthole
+%q{孔位测试  }
+end
+
+def  scmdalign
+%q{物件对齐  }
+end
+
+def  scmdmove
+%q{精确移动  }
+end
+
+def  schangematerial
+%q{更换材质  }
+end
+
+def  schangetexture
+%q{更换基材  }
+end
+
+
+def  stocompents
+%q{组成产品  }
+end
+
+def  scompentsname
+%q{产品名称  }
+end
+
+def  sspalesize
+%q{自动量距  }
+end
+
+def  scleargs
+%q{公式清理  }
+end
+
+def  xsdescription
+%q{工艺  }
+end
+
+
+def  xswidth
+%q{宽度  }
+end
+
+def  xsdepth
+%q{深度  }
+end
+
+def  xsheight
+%q{高度  }
+end
+
+def  xsxcd
+%q{X坐标  }
+end
+
+def  xsycd
+%q{Y坐标  }
+end
+
+def  xszcd
+%q{Z坐标  }
+end
+
+def  xsox
+%q{X角度  }
+end
+
+def  xsoy
+%q{Y角度  }
+end
+
+def  xsoz
+%q{Z角度  }
+end
+
+def  syoucantsetthebaseobj
+%q{您选择了相对位置摆放，在确定之前必须选择一个基准物件才可以的哦  }
+end
+
+def  syoucantselecttwobaseobj
+%q{请选择两个物件  }
+end
+
+def  drawaobjecterror
+%q{物件生成过程中发生错误，已被取消  }
+end
+
+def  drawaobjectok
+%q{物件已生成  }
+end
+
+def  getsplacesizeok
+%q{空间数据已获取  }
+end
+
+def  selobjtitle
+%q{家居产品选择  }
+end
+
+
+def  drawreporttitle
+%q{数据报表  }
+end
+
+
+def strunit
+%q{柜产品}
+end
+
+def strwj
+%q{五金}
+end
+
+def strdrawsys
+%q{系统图库  }
+end
+
+def strdrawlocal
+%q{本地图库  }
+end
+
+def strholeback
+%q{反转孔位  }
+end
+
+def strchangedir
+%q{纹路转向  }
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+file_loaded ("xtstring.rb")
